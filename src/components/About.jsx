c5c9a244b62e16c2574d0fc1079b2b04f7d250e5
kgotsosm <kgotsom@protonmail.com>
2023-12-01 00:53:46 +0200

    Edited About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,6 +1,6 @@
 // Purpose: About component for the portfolio website.
 import React from 'react';
-
+import shuttle from '../assets/shuttle.jpg';
 const About = () => {
   return (
     <div name="about" className="w-full h-screen bg-[#0a192f] text-gray-300">
@@ -15,12 +15,7 @@ const About = () => {
         </div>
         <div className="max-w-[1000px] p-4 md:p-0 w-full md:grid grid-cols-2 gap-8">
           <div className="mb-6 md:mb-0">
-            <p className="text-2xl md:text-4xl sm:ml-5 p-2 md:p-0 font-bold">
-              Welcome to my website, take a look around
-            </p>
-          </div>
-          <div>
-            <p className="px-2 lg:px-0">
+          <p className="px-2 lg:px-0 lg:py-14 lg:text-lg">
               My passion for learning and creating solutions with the latest
               technologies have guided my journey in tech. Combining my
               knowledge of full stack web development with data science
@@ -30,6 +25,9 @@ const About = () => {
               technologies and software development disciplines.
             </p>
           </div>
+          <div>
+          <img src={shuttle} alt="space shuttle" className="hidden lg:block h-42 object-contain" />        
+            </div>
         </div>
       </div>
     </div>
